f665e3f4c32efe01caa1fa1ea5aecf7b5292ab0d
Murilo Oliveira <muriloleiteoliveira1@gmail.com>
2026-01-02 18:53:10 -0300

    update

---
+++ 03-Funcoes/06-arrow-function/script.js
@@ -0,0 +1,11 @@
+let saudacao = () => {
+    console.log("Olá, mundo!")
+}
+
+console.log(saudacao())
+
+let saudacao2 = (nome) => {
+    console.log(`Olá, ${nome}!`)
+}
+
+console.log(saudacao2("Murilo"))
